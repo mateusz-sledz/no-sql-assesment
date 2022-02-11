98831b2251cd05a19d288277c72d8dd137611a4a
overc <ldandoy@overconsulting.net>
2022-02-11 16:16:18 +0100

    Add list part

--- index.js
+++ index.js
@@ -4,9 +4,38 @@ const express = require('express');
 require('./conf/database');
 
 const app = express();
+app.use(express.json());
+
+const CountryModel = require('./models/Country')
 
 app.get('/', (request, response) => {
     response.status(200).json({msg: "It works !"});
+});
+
+app.get('/countries', async (request, response) => {
+    const countries = await CountryModel.find();
+    response.status(200).json(countries);
+});
+
+app.get('/countries/:id', async (request, response) => {
+    const countryId = request.params.id;
+
+    const countries = await CountryModel.find({
+        _id: countryId
+    });
+    
+    response.status(200).json(countries);
+});
+
+app.post('/countries', async (request, response) => {
+    const {name, isoCode} = request.body
+
+    const country = await CountryModel.create({
+        name: name,
+        isoCode
+    });
+
+    response.status(200).json(country);
 });
 
 const port = 3000;
